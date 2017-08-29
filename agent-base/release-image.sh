447749863253345ee699db611acf09fb223feca4
Henrique Zambon <henrique.zambon@gmail.com>
2017-08-29 17:00:31 -0400

    Arch-specific images need to be pushed first

--- agent-base/release-image.sh
+++ agent-base/release-image.sh
@@ -6,11 +6,16 @@ TAG=${TAG:-dev}
 # Releasing from macOS requires setting REGISTRY_USERNAME and REGISRTY_PASSWORD
 # Ref: https://github.com/estesp/manifest-tool#sample-usage
 [ -n "$REGISTRY_USERNAME" ] && [ -n "$REGISTRY_PASSWORD" ] && \
-  AUTH="--username $REGISTRY_USERNAME --password $REGISTRY_PASSWORD"
+  AUTH=(--username "$REGISTRY_USERNAME" --password "$REGISTRY_PASSWORD")
 
 IMAGE=${REPO}/agent-base:${TAG}
 
-manifest-tool ${AUTH} push from-args \
+[ -n "${AUTH[*]}" ] && docker login -u "$REGISTRY_USERNAME" -p "$REGISTRY_PASSWORD"
+docker push ${IMAGE}_amd64
+docker push ${IMAGE}_ppc64le
+[ -n "${AUTH[*]}" ] && docker logout
+
+manifest-tool "${AUTH[@]}" push from-args \
   --platforms linux/amd64,linux/ppc64le \
   --template ${IMAGE}_ARCH \
   --target ${IMAGE}
