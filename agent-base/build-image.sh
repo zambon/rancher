bfbc41928eb642fb1a125d5752a599409562524d
Henrique Zambon <henrique.zambon@gmail.com>
2017-08-29 16:06:59 -0400

    arch arch

--- agent-base/build-image.sh
+++ agent-base/build-image.sh
@@ -4,10 +4,10 @@ REPO=${REPO:-rancher}
 TAG=${TAG:-dev}
 ARCH=${ARCH:-"$(docker version -f '{{.Server.Arch}}')"}
 
-IMAGE=${REPO}/agent-base:${TAG}_${ARCH}
+IMAGE=${REPO}/agent-base:${TAG}
 
-docker build --build-arg ARCH=${ARCH} -t ${IMAGE} .
-echo Built ${IMAGE}
+docker build --build-arg ARCH=${ARCH} -t ${IMAGE}_${ARCH} .
+echo Built ${IMAGE}_${ARCH}
 
 cat > image-manifest.yaml << EOF
 image: ${IMAGE}
