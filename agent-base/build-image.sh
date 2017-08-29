7c9354eb6cfd4e64edecf206ae00584eab6a6616
Henrique Zambon <henrique.zambon@gmail.com>
2017-08-29 11:22:40 -0400

    Use GOARCH

--- agent-base/build-image.sh
+++ agent-base/build-image.sh
@@ -3,5 +3,5 @@
 REPO=${REPO:-rancher}
 TAG=${TAG:-dev}
 
-docker build --build-arg ARCH=$(uname -m) -t $REPO/agent-base:${TAG} .
+docker build --build-arg ARCH=$(go env GOARCH) -t $REPO/agent-base:${TAG} .
 echo Built $REPO/agent-base:${TAG}
